6bad33d2eb2ace82433b22aab00ccbc871d9dac4
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2026-09-14 20:44:44 +0900

    more fixes

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -3,6 +3,7 @@ import * as migration_20260903_072603_add_tags_collection from "./20260903_07260
 import * as migration_20260914_194500_add_product_category from "./20260914_194500_add_product_category";
 import * as migration_20260914_194501_cleanup_unused_taxonomy from "./20260914_194501_cleanup_unused_taxonomy";
 import * as migration_20260914_194502_repair_product_category from "./20260914_194502_repair_product_category";
+import * as migration_20260914_194503_repair_product_category_column from "./20260914_194503_repair_product_category_column";
 
 export const migrations = [
 	{
@@ -29,5 +30,10 @@ export const migrations = [
 		up: migration_20260914_194502_repair_product_category.up,
 		down: migration_20260914_194502_repair_product_category.down,
 		name: "20260914_194502_repair_product_category",
+	},
+	{
+		up: migration_20260914_194503_repair_product_category_column.up,
+		down: migration_20260914_194503_repair_product_category_column.down,
+		name: "20260914_194503_repair_product_category_column",
 	},
 ];
